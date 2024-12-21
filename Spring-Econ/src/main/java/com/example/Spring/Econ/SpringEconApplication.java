b307cb4da1e82c895595f5d91f86f50c3c5fd921
Dhananjay <1rcrusoe1@gmail.com>
2024-12-21 08:27:25 +0530

    Added project folder

---
+++ Spring-Econ/src/main/java/com/example/Spring/Econ/SpringEconApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Econ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEconApplication.class, args);
+	}
+
+}
---
+++ Spring-Econ/src/test/java/com/example/Spring/Econ/SpringEconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Econ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
